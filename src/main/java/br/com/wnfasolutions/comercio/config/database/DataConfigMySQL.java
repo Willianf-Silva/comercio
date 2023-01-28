9a43e0b072c88e34bc62f1fe1308dc04186aee62
WillianF-Silva <willian.ferreira.da.silva@gmail.com>
2023-01-28 15:00:02 -0300

    Incluindo configuração database para profile dev

---
+++ src/main/java/br/com/wnfasolutions/comercio/config/database/DataConfigMySQL.java
@@ -0,0 +1,35 @@
+package br.com.wnfasolutions.comercio.config.database;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.vendor.Database;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+
+import javax.sql.DataSource;
+
+@Configuration
+@Profile("dev")
+public class DataConfigMySQL {
+
+    @Bean
+    public DataSource dataSource(){
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setUrl("jdbc:mysql://localhost/comercio?useTimezone=true&serverTimezone=UTC&useLegacyDatetimeCode=false&createDatabaseIfNotExist=true&allowPublicKeyRetrieval=true&useSSL=false");
+        dataSource.setUsername("root");
+        dataSource.setPassword("");
+        return dataSource;
+    }
+
+    public JpaVendorAdapter jpaVendorAdapter(){
+        HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
+        adapter.setDatabase(Database.MYSQL);
+        adapter.setShowSql(true);
+        adapter.setGenerateDdl(true);
+        adapter.setDatabasePlatform("org.hibernate.dialect.MySQLDialect");
+        adapter.setPrepareConnection(true);
+        return adapter;
+    }
+}
