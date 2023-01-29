01209569d1c76d1c42b16c2de65d7ff64cacc8ba
WillianF-Silva <willian.ferreira.da.silva@gmail.com>
2023-01-28 21:55:04 -0300

    Documentando endpoint cadastro de usuário

--- src/main/java/br/com/wnfasolutions/comercio/resource/swagger/UsuarioResourceSwagger.java
+++ src/main/java/br/com/wnfasolutions/comercio/resource/swagger/UsuarioResourceSwagger.java
@@ -1,8 +1,20 @@
 package br.com.wnfasolutions.comercio.resource.swagger;
 
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.http.ResponseEntity;
+
+import br.com.wnfasolutions.comercio.dto.request.UsuarioRequestDTO;
+import br.com.wnfasolutions.comercio.dto.response.UsuarioResponseDTO;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 
 @Api(tags = "Usuario")
 public interface UsuarioResourceSwagger {
 
+	@ApiOperation("Incluir um novo usuário.")
+	public ResponseEntity<UsuarioResponseDTO> cadastrarUsuario(
+			@ApiParam("Informações do usuários que será incluído.") UsuarioRequestDTO usuarioRequestDTO,
+			HttpServletResponse resp) throws Exception;
 }
