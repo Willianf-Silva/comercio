de0b07288557e53de26f1d7ab53fdc65489a1552
WillianF-Silva <willian.ferreira.da.silva@gmail.com>
2023-01-28 21:47:50 -0300

    Configurando time zone da aplicação

--- src/main/java/br/com/wnfasolutions/comercio/ComercioApplication.java
+++ src/main/java/br/com/wnfasolutions/comercio/ComercioApplication.java
@@ -1,13 +1,34 @@
 package br.com.wnfasolutions.comercio;
 
+import java.util.Locale;
+import java.util.TimeZone;
+
+import javax.annotation.PostConstruct;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.FixedLocaleResolver;
 
 @SpringBootApplication
 public class ComercioApplication {
-
+	private static final String TIME_ZONE = "America/Sao_Paulo";
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ComercioApplication.class, args);
 	}
 
+	/*
+	 * Método responsável por fixar a região que será tratato as datas, numero, etc
+	 */
+	@Bean
+	public LocaleResolver localeResolver() {
+		return new FixedLocaleResolver(new Locale("pt", "BR"));
+	}
+
+	@PostConstruct
+	void started(){
+		TimeZone.setDefault(TimeZone.getTimeZone(TIME_ZONE));
+	}
 }
